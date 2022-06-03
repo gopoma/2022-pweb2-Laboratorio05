44f7dd1cd2ecce1676ab538da471d36089f7b04f
Gustavo <gordono@unsa.edu.pe>
2022-06-03 09:25:59 -0500

    Structured Contacto model with other kinds of fields than TextField

--- src/contactos/models.py
+++ src/contactos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Contacto(models.Model):
+    isFamiliar = models.BooleanField(default=False)
+    fullname = models.CharField(max_length=255)
+    alias = models.CharField(max_length=255)
+    phoneNumber = models.IntegerField()
+    email = models.EmailField()
+    state = models.CharField(max_length=255, null=True)
+    city = models.CharField(max_length=255, null=True)
+    district = models.CharField(max_length=255, null=True)
