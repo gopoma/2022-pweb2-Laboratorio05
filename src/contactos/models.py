7bc6ba6ab13029b6fb1fe9c04ed1e2e52755edda
Gustavo <gordono@unsa.edu.pe>
2022-06-03 09:44:52 -0500

    I experimented what happens when you modify a register using an outer aplication from Django, it goes wrong :c

--- src/contactos/models.py
+++ src/contactos/models.py
@@ -7,9 +7,9 @@ class Contacto(models.Model):
     alias = models.CharField(max_length=255)
     phoneNumber = models.IntegerField()
     email = models.EmailField()
-    state = models.CharField(max_length=255, blank=True)
-    city = models.CharField(max_length=255, blank=True)
-    district = models.CharField(max_length=255, blank=True)
+    state = models.CharField(max_length=255, null=False)
+    city = models.CharField(max_length=255, null=False)
+    district = models.CharField(max_length=255, null=False)
 
     def __str__(self):
         return f"{self.fullname} ({'Yes' if self.isFamiliar else 'No'})"
