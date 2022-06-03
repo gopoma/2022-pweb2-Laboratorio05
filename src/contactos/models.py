e1f17858fc8c0f72397cc9686fe198faed4d2cae
Gustavo <gordono@unsa.edu.pe>
2022-06-03 10:53:52 -0500

    Reconstruct contactos app

---
+++ src/contactos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Contacto(models.Model):
+    isFamiliar = models.BooleanField(default=False)
+    fullname = models.CharField(max_length=255)
+    alias = models.CharField(max_length=255)
+    email = models.EmailField()
+    phoneNumber = models.IntegerField()
+    state = models.CharField(max_length=255, blank=True)
+    city = models.CharField(max_length=255, blank=True)
+    district = models.CharField(max_length=255, blank=True)
