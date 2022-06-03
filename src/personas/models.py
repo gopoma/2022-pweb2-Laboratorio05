7ca5fddd1ff6b1876c9d9cbad015ef1280704edb
Gustavo <gordono@unsa.edu.pe>
2022-06-02 22:55:16 -0500

    Maked a model named Personas, added it to the settings.py because I need to migrate that and prepared the migrations

--- src/personas/models.py
+++ src/personas/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    names = models.TextField()
+    surnames = models.TextField()
+    age = models.TextField()
