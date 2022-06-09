22b718922b32e57c6146b75d3bfdb3234818f059
Gustavo <gordono@unsa.edu.pe>
2022-06-09 09:59:46 -0500

    Se creó la aplicación inicio y se desarrolló la primera vista myHomeView, que responde un objeto del tipo HttpResponse

---
+++ src/inicio/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def myHomeView(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+
+    myContext = {
+        "myText": "Some text value",
+        "myNumber": 100,
+        "myList": [22, 33, 44, 55, "5to", "cuarto"]
+    }
+
+    return render(request, "home.html", myContext)
+
+def anotherView(request):
+    return HttpResponse("<h1>Another View!</h1>")
