980099db57c7d03d3e3764783014ca3cdf58e45a
Gustavo <gordono@unsa.edu.pe>
2022-06-23 22:54:47 -0500

    Django4-Diapositiva20: Se probo realizar la construcción del URL canónico con la función reverse() y el nombre que se le brinda a una URL al momento de mapearlo con una vista

--- src/personas/models.py
+++ src/personas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Persona(models.Model):
@@ -8,8 +9,9 @@ class Persona(models.Model):
     donador = models.BooleanField()
     # anotherField = models.BinaryField
 
+    # def get_absolute_url(self):
+    #     return f"/personas/{self.id}/"
     def get_absolute_url(self):
-        return f"/personas/{self.id}/"
-
+        return reverse("browsing", kwargs={"myID": self.id})
     def __str__(self):
         return f"{self.nombres} {self.apellidos}"
