a6e3fd78172a31e408c3d843c73f9f64de026b0c
Gustavo <gordono@unsa.edu.pe>
2022-06-23 21:17:23 -0500

    Django4-Diapositiva15: Previamente habiendo hecho la consulta por id, se realiza una capa de validación más, que es en caso de no encontrar el objeto que tenga cierto id a través del get_object_or_404

--- src/personas/views.py
+++ src/personas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Persona
 from .forms import PersonaForm, RawPersonaForm
 
@@ -78,8 +78,10 @@ def personaEditView(request):
         "form": form
     })
 
+# Go to localhost:8000/personas/<int:myID>
 def personasShowObject(request, myID):
-    persona = Persona.objects.get(id=myID)
+    # persona = Persona.objects.get(id=myID)
+    persona = get_object_or_404(Persona, id=myID)
     return render(request, "personas/description.html", {
         "persona": persona
     })
